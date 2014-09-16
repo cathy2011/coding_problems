8e6fd76819c799d83e74a6747c6c7cfcacf2e35f
QQ <QQ@QQ>
2014-09-16 19:46:45 -0400

    153 - Permalex (AC)

--- test/cpp/permalex.cc
+++ test/cpp/permalex.cc
@@ -2,20 +2,40 @@
 
 // Math
 
+// 14215110 153 Permalex  Accepted  C++11 0.016 2014-09-16 23:45:38
+
 #include <algorithm>
 #include <array>
 #include <functional>
 #include <iomanip>
 #include <iostream>
+#include <list>
+#include <numeric>
 #include <string>
 
 namespace {
 
-int Compute(const std::string& str) {
-  std::function<long long(int)> prod = [&prod](int k) -> long long {
-    return k <= 1 ? 1 : k * prod(k - 1);
+void Add(std::list<int>* check, std::list<int>* to, const std::list<int>& vec) {
+  std::function<int(int, int)> gcd = [&gcd](int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
   };
 
+  for (int value : vec) {
+    // For each value in 'vec', make sure it's prime relative to every element
+    // in 'check' before inserting it into 'to'.
+    for (auto it = check->begin(); it != check->end(); ++it) {
+      int tmp = gcd(*it, value);
+      *it /= tmp;
+      value /= tmp;
+    }
+    if (value > 1) {
+      to->push_back(value);
+    }
+  }
+}
+
+int Compute(const std::string& str) {
+  // Calculate frequency of characters in the given string.
   std::array<int, 26> freq;
   freq.fill(0);
   std::for_each(str.begin(), str.end(), [&freq](const char& ch) {
@@ -23,16 +43,24 @@ int Compute(const std::string& str) {
   });
 
   int precedes = 0;
-  for (int pos = 0; pos < static_cast<int>(str.length()); ++pos) {
+  for (int pos = 0; pos < static_cast<int>(str.length()) - 1; ++pos) {
     // Compute #of permutations starting from pos + 1.
-    long long p = prod(str.length() - pos);
+    std::list<int> up(static_cast<int>(str.length()) - 1 - pos), down;
+    std::iota(up.begin(), up.end(), 1);
     std::for_each(freq.begin(), freq.end(), [&](const int& c) {
-      p /= prod(c);
-    });
+                    // Add 1...c to 'down'
+                    std::list<int> tmp(c);
+                    std::iota(tmp.begin(), tmp.end(), 1);
+                    Add(&up, &down, tmp);
+                  });
+    long long up_prod = std::accumulate(up.begin(), up.end(), 1,
+                                        std::multiplies<long long>());
+    long long down_prod = std::accumulate(down.begin(), down.end(), 1,
+                                          std::multiplies<long long>());
 
     std::for_each(freq.begin(), freq.begin() + str.at(pos) - 'a',
                   [&](const int& c) {
-                    precedes += p * c / (str.length() - pos);
+                    precedes += up_prod * c / down_prod;
                   });
 
     --freq[str.at(pos) - 'a'];
